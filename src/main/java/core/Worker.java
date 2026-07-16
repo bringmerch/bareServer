f202ac6ad8bfb69a4ac5508bfce5be8c1868d456
bringmerch <lgop8327@gmail.com>
2026-07-16 13:12:18 +0900

    임시커밋

--- src/main/java/core/Worker.java
+++ src/main/java/core/Worker.java
@@ -23,6 +23,7 @@ public interface Worker<T> {
     void execute(Request request, DataProcessor dataProcessor) throws IOException;
 
     Response doProcess(Request request) throws IOException;
+
     T doGet(Request request) throws IOException;
 
     void doPost() throws IOException;
@@ -31,26 +32,36 @@ public interface Worker<T> {
 
     void doDelete() throws IOException;
 
-    default Response getErrorResponse(Request request) throws FileNotFoundException {
+    default Response getErrorResponse(Request request) throws IOException {
         Response<String> response = new Response(request.getResponseStatusCode());
         response.addHeader(new Header("Content-Type", ContentType.TEXT_HTML.value));
 
         FileManager fileManager = new FileManager();
-        File file = fileManager.loadFile(ContentType.TEXT_HTML.resourceDir + "/error/" + request.getResponseStatusCode() + ContentType.TEXT_HTML.extension);
-        FileInputStream fileInputStream = new FileInputStream(file);
-        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, StandardCharsets.UTF_8);
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
 
         String line;
         String responseBody = "";
 
+        // 1. null 선언
+
         try {
+            //2. work
+            File file = fileManager.loadFile(ContentType.TEXT_HTML.resourceDir + "/error/" + request.getResponseStatusCode() + ContentType.TEXT_HTML.extension);
+            FileInputStream fileInputStream = new FileInputStream(file);
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, StandardCharsets.UTF_8);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            // if null ? throw RuntimeException () : go!
+
             while ((line = bufferedReader.readLine()) != null) {
                 responseBody += line;
             }
         } catch (IOException e) {
-            e.printStackTrace();
-            return null;
+            throw e;
+        } finally {
+            // 3. 여전히 null이면 안댐
+            if ()
+            bufferedReader.close();
         }
 
         response.setBody(responseBody);
