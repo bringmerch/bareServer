cc1d353d099189cea3277104cb1293aa609957c4
bringmerch <lgop8327@gmail.com>
2026-07-27 10:05:46 +0900

    서버안죽게

--- src/main/java/core/Connector.java
+++ src/main/java/core/Connector.java
@@ -1,23 +1,31 @@
 package core;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Connector {
     public void start() throws Exception {
         ServerSocket serverSocket;
-        serverSocket = new ServerSocket(8080);
+        serverSocket = new ServerSocket(8081);
 
         while (!serverSocket.isClosed()) {
             ConnectionHandler handler = new ConnectionHandler();
-            Socket clientSocket = serverSocket.accept();
+            Socket clientSocket = null;
+            OutputStream outputStream = null;
             try {
+                clientSocket = serverSocket.accept();
                 handler.handle(clientSocket);
-            } catch (BareException | IOException | IllegalArgumentException e) {
+            } catch (IllegalArgumentException | IOException | BareException e) {
                 System.out.println("connection handling failed: " + e.getMessage());
+                try {
+                    if (clientSocket != null && (outputStream = clientSocket.getOutputStream()) != null)
+                        Writer.write(ErrorResponse.create(500), outputStream);
+                } catch (Exception exception) {}
             } finally {
                 ResourceCloser.close(clientSocket);
+                ResourceCloser.close(outputStream);
             }
         }
         ResourceCloser.close(serverSocket);
