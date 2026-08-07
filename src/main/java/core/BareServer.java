7b3dce18cdc5c1637ee6ae6a04d4da2a45db5d29
bringmerch <lgop8327@gmail.com>
2026-08-07 17:56:38 +0900

    ??????????????

--- src/main/java/core/BareServer.java
+++ src/main/java/core/BareServer.java
@@ -22,6 +22,7 @@ public class BareServer {
     // application start
     public static void main(String[] args) throws Exception {
         try {
+            System.out.println("안녕....");
             new Connector().listen(new ApplicationContext());
         } catch (Exception e) {
             e.printStackTrace();
